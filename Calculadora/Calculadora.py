ee13ee01c3f70438097b119c54a2f76e0b855c38
GustavoRSL <d7gustavo@yahoo.com.br>
2021-05-18 22:05:33 -0300

    Criação da janela principal(Em branco) do projeto calculadora.

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -1,1 +1,23 @@
-:)
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class JanelaPrincipal(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.top = 500
+        self.left = 500
+        self.height = 500
+        self.width = 200
+        self.tittle = "Caculadora"
+        self.load_main_screen()
+
+    def load_main_screen(self):
+        self.setGeometry(self.left, self.top, self.height, self.width)
+        self.setWindowTitle(self.tittle)
+        self.show()
+    
+if __name__ == '__main__':
+    aplication = QApplication(sys.argv)
+    window = JanelaPrincipal()
+    sys.exit(aplication.exec_())
